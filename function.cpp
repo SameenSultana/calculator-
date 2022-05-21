9cdeb41cc54ea79c03828657b43bbb2e8fa2c6a3
SameenSultana <103851138+SameenSultana@users.noreply.github.com>
2022-05-20 20:36:55 -0700

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int valueOne =10;
+int valueTwo = 20;
+
+
+void Add();
+int main()
+
+{
+       Add();
+       
+}
+void Add()
+{
+	cout<<"the sum of two integers is -->"<<valueOne + valueTwo<<endl;
+}
