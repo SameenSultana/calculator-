aadc364011c719cc07fec39f22a76810aa9d20dc
SameenSultana <103851138+SameenSultana@users.noreply.github.com>
2022-05-20 20:25:52 -0700

    Create if-else.cpp

---
+++ if-else.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=1;
+	int y=3;
+	int z=9;
+	
+	if(x==y||y==z&&x==z)
+	{
+		cout<<"the variabe are equal";
+	}
+	
+	if(x!=y)
+	{
+		cout<<"the variable are not equal";
+	}
+	
+	else
+	{
+		cout<<"the body of else";
+	}
+}
